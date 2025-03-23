3ff8382d5ca0c1a5a2ba93e1a7a7026ff49e8de1
kacperramenda <kacper.ramenda1@gmail.com>
2025-03-23 13:38:44 +0100

    chore: remove not used default files

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,7 +5,6 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
