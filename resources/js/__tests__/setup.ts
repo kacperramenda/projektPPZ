1db2f63f096674b0cb6e2aacc168221eeb2f10e5
Mroki888 <mikolaj.rokitowski@interia.pl>
2025-04-09 00:13:20 +0200

    Revert "feat: integration-test"
    This reverts commit 8f738b2fb2497635f467fff1de06578e89479520.

--- resources/js/__tests__/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-import { cleanup } from '@testing-library/vue'
-import { afterEach } from 'vitest'
-
-// Cleanup after each test
-afterEach(() => {
-  cleanup()
-})
