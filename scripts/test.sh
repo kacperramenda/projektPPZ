fb8aabf07918c75abb60309cd57a6248ba77e4a3
Paweł <neverseen1997@gmail.com>
2025-04-30 13:20:14 +0200

    readme do testów

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,12 @@
+# migrowanie bazy testowej
+php artisan migrate:fresh --env=testing
+php artisan migrate --env=testing
+
+# odpalenie wszystkich testów w projekcie
+php artisan test
+
+# odpalenie pojedynczego pliku z testami
 php artisan test --filter=AuthenticationTest
+
+# weryfikacja migracji
+php artisan migrate:status
